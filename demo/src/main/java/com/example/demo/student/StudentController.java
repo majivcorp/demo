733e0febc6ac3572f256504123fd6e7ffd4b425f
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-06-21 14:31:42 -0400

    Dependency injection comments

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//All of the resources for our API Layer
 @RestController
 @RequestMapping(path ="api/v1/student")
 
 public class StudentController {
 
-    private final StudentService studentService;
+    private final StudentService studentService; // Student service is to be injected into the constructor below, once instantiated from StudentService
 
 @Autowired
     public StudentController(StudentService studentService) {
